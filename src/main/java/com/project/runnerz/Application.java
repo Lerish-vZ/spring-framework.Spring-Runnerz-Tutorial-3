aa4a0baeed613f5410dbe09361a497464b564ae8
Lerish-vZ <lerishavz@gmail.com>
2025-01-28 12:50:36 +0200

    update

--- src/main/java/com/project/runnerz/Application.java
+++ src/main/java/com/project/runnerz/Application.java
@@ -1,17 +1,18 @@
 package com.project.runnerz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
-		System.out.println(welcomeMessage);
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		log.info("Application started successfully");
 	}
 
 }
