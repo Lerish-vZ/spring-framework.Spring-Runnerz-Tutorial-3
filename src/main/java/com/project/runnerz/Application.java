2b90cfdd62b6b2f0c33896888f7a037d2d9253b4
Lerish-vZ <lerishavz@gmail.com>
2025-01-28 13:13:19 +0200

    update

--- src/main/java/com/project/runnerz/Application.java
+++ src/main/java/com/project/runnerz/Application.java
@@ -1,9 +1,16 @@
 package com.project.runnerz;
 
+import com.project.runnerz.run.Location;
+import com.project.runnerz.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class Application {
@@ -12,7 +19,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		log.info("Application started successfully");
 	}
 
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+		}
+	}
 }
