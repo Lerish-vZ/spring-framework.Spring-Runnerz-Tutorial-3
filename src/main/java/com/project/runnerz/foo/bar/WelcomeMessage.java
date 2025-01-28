2395ef94bc035262bf7e6b15fe718a5946c6db46
Lerish-vZ <lerishavz@gmail.com>
2025-01-28 12:41:22 +0200

    update

--- src/main/java/com/project/runnerz/foo/bar/WelcomeMessage.java
+++ src/main/java/com/project/runnerz/foo/bar/WelcomeMessage.java
@@ -1,4 +1,10 @@
 package com.project.runnerz.foo.bar;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class WelcomeMessage {
+    public String getWelcomeMessage() {
+        return "Welcome to Spring Runnerz";
+    }
 }
