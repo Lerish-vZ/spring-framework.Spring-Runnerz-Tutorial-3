e36293e792df6334fe90a13d47a29d57696b5b1e
Lerish-vZ <lerishavz@gmail.com>
2025-01-28 12:47:40 +0200

    update

--- src/main/java/com/project/runnerz/Application.java
+++ src/main/java/com/project/runnerz/Application.java
@@ -2,13 +2,16 @@ package com.project.runnerz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
+		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
+		System.out.println(welcomeMessage);
 	}
 
 }
