5a54c9c81bd4caddb6bb4d6d86f626516c3c24c5
Lerish-vZ <lerishavz@gmail.com>
2025-01-28 13:07:57 +0200

    update

--- src/main/java/com/project/runnerz/run/Run.java
+++ src/main/java/com/project/runnerz/run/Run.java
@@ -1,4 +1,13 @@
 package com.project.runnerz.run;
 
-public record Run() {
+import java.time.LocalDateTime;
+
+public record Run(
+        Integer id,
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
+        Integer miles,
+        Location location
+) {
 }
