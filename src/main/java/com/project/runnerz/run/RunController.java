fbc003de3182432af6362e0a63fbb56a632ea485
Lerish-vZ <lerishavz@gmail.com>
2025-01-29 09:59:06 +0200

    update

--- src/main/java/com/project/runnerz/run/RunController.java
+++ src/main/java/com/project/runnerz/run/RunController.java
@@ -1,4 +1,13 @@
 package com.project.runnerz.run;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RunController {
+
+    @GetMapping("/hello")
+    String home(){
+        return "Hello, Runnerz!";
+    }
 }
