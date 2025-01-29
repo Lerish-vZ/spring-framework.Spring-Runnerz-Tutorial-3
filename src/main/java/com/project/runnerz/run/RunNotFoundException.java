096916adc456669bd8968ba4a1ea91e8d401dba3
Lerish-vZ <lerishavz@gmail.com>
2025-01-29 11:56:27 +0200

    update

--- src/main/java/com/project/runnerz/run/RunNotFoundException.java
+++ src/main/java/com/project/runnerz/run/RunNotFoundException.java
@@ -1,4 +1,11 @@
 package com.project.runnerz.run;
 
-public class RunNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RunNotFoundException extends RuntimeException {
+    public RunNotFoundException() {
+        super("Run Not Found");
+    }
 }
