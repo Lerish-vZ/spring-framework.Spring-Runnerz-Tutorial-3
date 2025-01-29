9d2d67126fe8268e65cf7a21c4048486e21478ad
Lerish-vZ <lerishavz@gmail.com>
2025-01-29 11:41:03 +0200

    update

--- src/main/java/com/project/runnerz/run/RunController.java
+++ src/main/java/com/project/runnerz/run/RunController.java
@@ -1,7 +1,6 @@
 package com.project.runnerz.run;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
