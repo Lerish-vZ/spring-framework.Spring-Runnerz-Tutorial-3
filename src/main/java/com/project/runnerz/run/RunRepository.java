b18ae1e3cb5a33254c5e3863b0e4f4960afbbd20
Lerish-vZ <lerishavz@gmail.com>
2025-01-29 10:04:45 +0200

    update

--- src/main/java/com/project/runnerz/run/RunRepository.java
+++ src/main/java/com/project/runnerz/run/RunRepository.java
@@ -1,5 +1,9 @@
 package com.project.runnerz.run;
 
+import jakarta.annotation.PostConstruct;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,5 +11,17 @@ public class RunRepository {
 
     private List<Run> runs = new ArrayList<>();
 
-    
+    List<Run> findAll() {
+        return runs;
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR));
+    }
 }
