8b93d59ace11fd6e4aeace580bef411e6c190919
Lerish-vZ <lerishavz@gmail.com>
2025-01-29 11:18:35 +0200

    update

--- src/main/java/com/project/runnerz/run/RunController.java
+++ src/main/java/com/project/runnerz/run/RunController.java
@@ -1,6 +1,7 @@
 package com.project.runnerz.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +22,8 @@ public class RunController {
         return runRepository.findAll();
     }
 
-    @GetMapping("/1")
-    Run findById(){
-        return runRepository.findById(1);
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        return runRepository.findById(id);
     }
 }
