26719cc508322531acf5f246215b9210d8419511
Lerish-vZ <lerishavz@gmail.com>
2025-01-29 10:02:20 +0200

    update

--- src/main/java/com/project/runnerz/run/RunRepository.java
+++ src/main/java/com/project/runnerz/run/RunRepository.java
@@ -1,4 +1,11 @@
 package com.project.runnerz.run;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    
 }
