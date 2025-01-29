a7249907730227f9fcdedeb3c3965e18f5d4002a
Lerish-vZ <lerishavz@gmail.com>
2025-01-29 11:04:29 +0200

    update

--- src/main/java/com/project/runnerz/run/RunController.java
+++ src/main/java/com/project/runnerz/run/RunController.java
@@ -3,13 +3,19 @@ package com.project.runnerz.run;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RunController {
 
+    private final RunRepository runRepository;
 
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
 
-    @GetMapping("/hello")
-    String home(){
-        return "Hello, Runnerz!";
+    @GetMapping("/api/runs")
+    List<Run> findAll(){
+        return runRepository.findAll();
     }
 }
