9e486803c1dc29e7a2ea6f9cb8e931f8c8b7e6a0
Lerish-vZ <lerishavz@gmail.com>
2025-01-31 09:29:25 +0200

    update

--- src/main/java/com/project/runnerz/run/RunController.java
+++ src/main/java/com/project/runnerz/run/RunController.java
@@ -44,7 +44,7 @@ public class RunController {
     //PUT
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
