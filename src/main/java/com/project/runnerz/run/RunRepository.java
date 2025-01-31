94b1641394b9ba87ffd3c56d5c28f741a5791a23
Lerish-vZ <lerishavz@gmail.com>
2025-01-31 10:56:54 +0200

    update

--- src/main/java/com/project/runnerz/run/RunRepository.java
+++ src/main/java/com/project/runnerz/run/RunRepository.java
@@ -14,46 +14,5 @@ public class RunRepository {
 
     private List<Run> runs = new ArrayList<>();
 
-    List<Run> findAll() {
-        return runs;
-    }
 
-    Optional<Run> findById(Integer id) {
-       return Optional.of(runs.stream()
-               .filter(run -> run.id().equals(id))
-               .findFirst()
-               .get());
-    }
-
-    void create(Run run) {
-        runs.add(run);
-    }
-
-    void update(Run run, Integer id) {
-        Optional<Run> existingRun = findById(id);
-        if (existingRun.isPresent()) {
-            runs.set(runs.indexOf(existingRun.get()), run);
-        }
-    }
-
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
-    }
-
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1,
-                "Monday Morning Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
-                3,
-                Location.INDOOR));
-
-        runs.add(new Run(2,
-                "Wednesday Evening Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
-                6,
-                Location.INDOOR));
-    }
 }
