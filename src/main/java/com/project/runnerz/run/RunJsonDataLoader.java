3ff40e9ed6b77adc0ff3e2d3959f9de1faadb5a8
Lerish-vZ <lerishavz@gmail.com>
2025-02-03 12:22:08 +0200

    update

--- src/main/java/com/project/runnerz/run/RunJsonDataLoader.java
+++ src/main/java/com/project/runnerz/run/RunJsonDataLoader.java
@@ -26,7 +26,7 @@ public class RunJsonDataLoader implements CommandLineRunner {
         if(runRepository.count() == 0){
             try(InputStream inputStream = TypeReference.class.getResourceAsStream("/data/run.json")){
                 Runs allRuns = objectMapper.readValue(inputStream, Runs.class);
-                log.info("Reading {} runs from JSON data and saving to in-memory collection.", allRuns.runs().size());
+                log.info("Reading {} runs from JSON data and saving it to a database.", allRuns.runs().size());
                 runRepository.saveAll(allRuns.runs());
             } catch (IOException e){
                 throw new RuntimeException("Failed to read JSON data", e);
