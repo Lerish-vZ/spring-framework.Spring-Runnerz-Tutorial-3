a7caa3f896cce506822aeaee7f982262fab02ac2
Lerish-vZ <lerishavz@gmail.com>
2025-02-03 09:46:10 +0200

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO Run (id, title, started_on, completed_on, miles, location)
-VALUES (1, 'Morning Run', '2025-01-01 06:00:00', '2025-01-01 7:00:00', 5, 'OUTDOOR')
+INSERT INTO Run (title, started_on, completed_on, miles, location)
+VALUES ('Morning Run', '2025-01-01 06:00:00', '2025-01-01 07:00:00', 5, 'OUTDOOR');
