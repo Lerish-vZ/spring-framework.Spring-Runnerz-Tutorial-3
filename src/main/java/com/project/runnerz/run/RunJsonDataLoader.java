af1d4b12143338c73c27d4245e1a55c5d87f1365
Lerish-vZ <lerishavz@gmail.com>
2025-02-03 12:13:55 +0200

    update

--- src/main/java/com/project/runnerz/run/RunJsonDataLoader.java
+++ src/main/java/com/project/runnerz/run/RunJsonDataLoader.java
@@ -2,7 +2,12 @@ package com.project.runnerz.run;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.asm.TypeReference;
 import org.springframework.boot.CommandLineRunner;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class RunJsonDataLoader implements CommandLineRunner {
 
@@ -15,6 +20,16 @@ public class RunJsonDataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        if(runRepository.count() == 0){
+            try(InputStream inputStream = TypeReference.class.getResourceAsStream("/data/run.json")){
+                Runs allRuns = objectMapper.readValue(inputStream, Runs.class);
+                log.info("Reading {} runs from JSON data and saving to in-memory collection.", allRuns.runs().size());
+                runRepository.saveAll(allRuns.runs());
+            } catch (IOException e){
+                throw new RuntimeException("Failed to read JSON data", e);
+            }
+        } else {
+            log.info("Not loading Runs from JSON data because the collection contains data.");
+        }
     }
 }
