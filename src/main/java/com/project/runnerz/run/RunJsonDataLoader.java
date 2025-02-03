f7f06cfd5e4a9d7236e9e91e335e8f12dcedcda6
Lerish-vZ <lerishavz@gmail.com>
2025-02-03 12:14:26 +0200

    update

--- src/main/java/com/project/runnerz/run/RunJsonDataLoader.java
+++ src/main/java/com/project/runnerz/run/RunJsonDataLoader.java
@@ -1,5 +1,6 @@
 package com.project.runnerz.run;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.asm.TypeReference;
@@ -13,8 +14,9 @@ public class RunJsonDataLoader implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(RunJsonDataLoader.class);
     private final RunRepository runRepository;
+    private final ObjectMapper objectMapper;
 
-    public RunJsonDataLoader(RunRepository runRepository) {
+    public RunJsonDataLoader(RunRepository runRepository, ObjectMapper objectMapper) {
         this.runRepository = runRepository;
     }
 
