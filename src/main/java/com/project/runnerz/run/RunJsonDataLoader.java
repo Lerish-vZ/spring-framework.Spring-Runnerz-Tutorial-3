38cad9574e47c06c44b0ad14011aeb273b1b2dc8
Lerish-vZ <lerishavz@gmail.com>
2025-02-03 12:07:33 +0200

    update

--- src/main/java/com/project/runnerz/run/RunJsonDataLoader.java
+++ src/main/java/com/project/runnerz/run/RunJsonDataLoader.java
@@ -1,4 +1,20 @@
 package com.project.runnerz.run;
 
-public class RunJsonDataLoader {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+
+public class RunJsonDataLoader implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(RunJsonDataLoader.class);
+    private final RunRepository runRepository;
+
+    public RunJsonDataLoader(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
