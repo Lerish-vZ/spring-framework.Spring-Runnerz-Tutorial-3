bd7a524bbafc06a5efb5b06ed22628a4996c86c6
Lerish-vZ <lerishavz@gmail.com>
2025-02-20 10:48:43 +0200

    update

--- src/main/java/com/project/runnerz/user/UserRestClient.java
+++ src/main/java/com/project/runnerz/user/UserRestClient.java
@@ -6,5 +6,10 @@ import org.springframework.web.client.RestClient;
 @Component
 public class UserRestClient {
     private final RestClient restClient;
-    
+
+    public UserRestClient(RestClient.Builder builder) {
+        this.restClient = builder
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .build();
+    }
 }
