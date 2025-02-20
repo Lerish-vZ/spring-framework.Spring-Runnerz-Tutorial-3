6ddae6a49b0835792dd6a2deae5369c209e0ccee
Lerish-vZ <lerishavz@gmail.com>
2025-02-20 10:46:59 +0200

    update

--- src/main/java/com/project/runnerz/user/UserRestClient.java
+++ src/main/java/com/project/runnerz/user/UserRestClient.java
@@ -1,4 +1,10 @@
 package com.project.runnerz.user;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
 public class UserRestClient {
+    private final RestClient restClient;
+    
 }
