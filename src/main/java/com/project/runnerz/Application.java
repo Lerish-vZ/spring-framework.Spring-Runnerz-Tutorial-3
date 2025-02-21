ad1e334b4131b8af6e6b85e395cd7bcf29100444
Lerish-vZ <lerishavz@gmail.com>
2025-02-21 10:54:11 +0200

    update

--- src/main/java/com/project/runnerz/Application.java
+++ src/main/java/com/project/runnerz/Application.java
@@ -1,6 +1,7 @@
 package com.project.runnerz;
 
 import com.project.runnerz.user.User;
+import com.project.runnerz.user.UserHttpClient;
 import com.project.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -18,6 +22,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
 	}
 
 	@Bean
