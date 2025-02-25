7430fd972e2f25323ffb792f0bd7bdfed312b89f
Lerish-vZ <lerishavz@gmail.com>
2025-02-25 13:51:54 +0200

    update

--- src/test/java/com/project/runnerz/run/InMemoryRunRepositoryTest.java
+++ src/test/java/com/project/runnerz/run/InMemoryRunRepositoryTest.java
@@ -1,7 +1,32 @@
 package com.project.runnerz.run;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryRunRepositoryTest {
+    InMemoryRunRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new InMemoryRunRepository();
+        repository.create(new Run ( 1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR, null));
+
+        repository.create(new Run ( 2,
+                "Wednesday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+                6,
+                Location.INDOOR, null));
+    }
+
 
 }
