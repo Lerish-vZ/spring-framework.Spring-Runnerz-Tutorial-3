aba9645f3a8b9c14b3aae9df4d8907d89005d8d9
Lerish-vZ <lerishavz@gmail.com>
2025-02-25 13:58:52 +0200

    update

--- src/test/java/com/project/runnerz/run/InMemoryRunRepositoryTest.java
+++ src/test/java/com/project/runnerz/run/InMemoryRunRepositoryTest.java
@@ -35,4 +35,56 @@ class InMemoryRunRepositoryTest {
         List<Run> runs = repository.findAll();
         assertEquals(2, runs.size(), "Should find 2 runs");
     }
+
+    @Test
+    void shouldFindRunWithValidId() {
+        var run = repository.findById(1).get();
+        assertEquals("Monday Morning Run", run.title());
+        assertEquals(3, run.miles());
+    }
+
+    @Test
+    void shouldNotFindRunWithInvalidId() {
+        RunNotFoundException notFoundException = assertThrows(
+                RunNotFoundException.class,
+                () -> repository.findById(3).get()
+        );
+
+        assertEquals("Run Not Found", notFoundException.getMessage());
+    }
+
+    @Test
+    void shouldCreateNewRun() {
+        repository.create(new Run(3,
+                "Friday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR , null));
+        List<Run> runs = repository.findAll();
+        assertEquals(3, runs.size());
+    }
+
+    @Test
+    void shouldUpdateRun() {
+        repository.update(new Run(1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                5,
+                Location.OUTDOOR, null), 1);
+        var run = repository.findById(1).get();
+        assertEquals("Monday Morning Run", run.title());
+        assertEquals(5, run.miles());
+        assertEquals(Location.OUTDOOR, run.location());
+    }
+
+    @Test
+    void shouldDeleteRun() {
+        repository.delete(1);
+        List<Run> runs = repository.findAll();
+        assertEquals(1, runs.size());
+    }
+
+
 }
